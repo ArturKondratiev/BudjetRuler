30029ec92840594d4f648a11f89d310b67f214d1
konartur <suntikflood@gmail.com>
2024-01-26 01:44:52 +0200

    Created draft header

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
-import { NativeBaseProvider } from 'native-base';
+import { NativeBaseProvider, View, useColorModeValue } from 'native-base';
 import React from 'react';
 
-import Test from '~/components/Test';
+import AppBar from '~/components/AppBar/AppBar';
 
 export default function App() {
+  const bg = useColorModeValue('coolGray.50', 'coolGray.800');
+  const color = useColorModeValue('coolGray.800', 'coolGray.50');
   return (
     <NativeBaseProvider>
-      <Test />
+      <View bgColor={bg} color={color} height="100%">
+        <AppBar />
+      </View>
     </NativeBaseProvider>
   );
 }
