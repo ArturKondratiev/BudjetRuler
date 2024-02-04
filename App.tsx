75bd3dbd7aedd3b2daf79840f2357e683382d035
Artur Kondratiev <138689962+ArturKondratiev@users.noreply.github.com>
2024-02-04 21:59:45 +0200

    Set up 1 (#3)
    * [SET-UPP-1] Create an folder architecture inside project
    
    * Created draft header
    
    * switched to gluestack-ui
    
    * created UI for calc budjet
    
    ---------
    
    Co-authored-by: konartur <suntikflood@gmail.com>

--- App.tsx
+++ App.tsx
@@ -1,16 +1,16 @@
-import { NativeBaseProvider, View, useColorModeValue } from 'native-base';
-import React from 'react';
+import { config } from '@gluestack-ui/config';
+import { GluestackUIProvider } from '@gluestack-ui/themed';
 
-import AppBar from '~/components/AppBar/AppBar';
+import { Header, Main } from '~/components';
+
 
 export default function App() {
   const bg = useColorModeValue('coolGray.50', 'coolGray.800');
   const color = useColorModeValue('coolGray.800', 'coolGray.50');
   return (
-    <NativeBaseProvider>
-      <View bgColor={bg} color={color} height="100%">
-        <AppBar />
-      </View>
-    </NativeBaseProvider>
+    <GluestackUIProvider config={config}>
+      <Header />
+      <Main />
+    </GluestackUIProvider>
   );
 }
---
+++ src/components/styles.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    paddingVertical: 16,
+    paddingHorizontal: 12,
+    borderRadius: 5,
+    alignSelf: 'center',
+    width: 375,
+    maxWidth: '100%',
+  },
+});
