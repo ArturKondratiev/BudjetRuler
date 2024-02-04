cdf1775467b47308eae0f7a09f5e36c4ce2d610d
konartur <suntikflood@gmail.com>
2024-02-04 22:01:41 +0200

    removed redundant code

--- App.tsx
+++ App.tsx
@@ -3,10 +3,7 @@ import { GluestackUIProvider } from '@gluestack-ui/themed';
 
 import { Header, Main } from '~/components';
 
-
 export default function App() {
-  const bg = useColorModeValue('coolGray.50', 'coolGray.800');
-  const color = useColorModeValue('coolGray.800', 'coolGray.50');
   return (
     <GluestackUIProvider config={config}>
       <Header />
